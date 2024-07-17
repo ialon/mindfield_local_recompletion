27e139bf625c75072dc4c8f62d5eca490387c925
Dan Marsden <dan@danmarsden.com>
2024-07-18 10:11:51 +1200

    Update version.php

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2023112706;
-$plugin->release   = 2023112706;
+$plugin->version   = 2023112707;
+$plugin->release   = 2023112707;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2022112805; // Requires 4.1.
 $plugin->component = 'local_recompletion';
